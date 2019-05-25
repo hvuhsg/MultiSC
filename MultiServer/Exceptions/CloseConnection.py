018df8944f6e28971de3def30bdac802baae99ba
hvuhsg <hvuhsg6@gmail.com>
2019-05-25 23:27:04 +0300

    delete unuse files

--- MultiServer/Exceptions/CloseConnection.py
+++
@@ -1,7 +0,0 @@
-from .MainException import MainException
-
-
-class CloseConnection(MainException):
-    def __init__(self, message=""):
-        self.level = 0
-        super(CloseConnection, self).__init__(message, self.level)
--- MultiServer/quick_setup/manager.py
+++
@@ -1,6 +0,0 @@
-from ..quick_setup import ProtocolsManager
-from ..quick_setup import MonitorManager
-from ..run import Runner
-
-
-__all__ = ["Runner", "ProtocolsManager", "MonitorManager"]

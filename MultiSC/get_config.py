587efae09429e14675659f9701a91b842ea75509
hvuhsg <hvuhsg6@gmail.com>
2019-06-03 00:47:22 +0300

    finel fix get_config command

--- MultiSC/get_config.py
+++ MultiSC/get_config.py
@@ -10,16 +10,20 @@ def copyanything(src, dst):
 
 
 def main():
+    from os import path
     from sys import argv
+    
+    dir_path = path.dirname(__file__)
+    
     if len(argv) != 2:
         print("get one argument <server|client>")
     if argv[1] not in ("server", "client"):
         print("get one argument <server|client>")
     
     if argv[1] == "server":
-        copyanything("MultiServer/__config__", "./__config__")
+        copyanything(path.join(dir_path, "MultiServer/__config__") , "./__config__")
     elif argv[1] == "client":
-        copyanything("MultiClient/__client_config__", "./__client_config__")
+        copyanything(path.join(dir_path, "MultiClient/__client_config__"), "./__client_config__")
 
 if __name__ == '__main__':
     main()

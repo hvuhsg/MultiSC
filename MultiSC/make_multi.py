9d2dfbba8abada3d19f0894248b0dfb07d9152a9
hvuhsg <hvuhsg6@gmail.com>
2019-06-07 14:25:53 +0300

    setup and create project update

--- MultiSC/make_multi.py
+++ MultiSC/make_multi.py
@@ -1,30 +1,46 @@
-import shutil, errno
+import shutil
 
-def copyanything(src, dst):    
+
+def copyanything(src, dst):
     shutil.copytree(src, dst)
-
 
 
 def main():
     from os import path
     from sys import argv
-    
+
     dir_path = path.dirname(__file__)
-    
-    if len(argv) != 2:
+
+    if len(argv) < 2 or argv[1] not in ("server", "client"):
         print("get one argument <server|client>")
-    if argv[1] not in ("server", "client"):
-        print("get one argument <server|client>")
-    
+
     try:
         if argv[1] == "server":
-            copyanything(path.join(dir_path, "MultiServer/__config__") , "./__config__")
-            shutil.copyfile(path.join(dir_path, "Examples/simple_server.py") , "./simple_server.py")
+            copyanything(path.join(dir_path, "MultiServer/__config__"), "./__config__")
+            if len(argv) > 2 and argv[2] in ("--quick", "-q"):
+                shutil.copyfile(
+                    path.join(dir_path, "Examples/simple_quick_server.py"),
+                    "./simple_quick_server.py",
+                )
+            else:
+                copyanything(path.join(dir_path, "Examples/protocols"), "./protocols")
+                shutil.copyfile(
+                    path.join(dir_path, "Examples/simple_server.py"),
+                    "./simple_server.py",
+                )
         elif argv[1] == "client":
-            copyanything(path.join(dir_path, "MultiClient/__client_config__"), "./__client_config__")
-            shutil.copyfile(path.join(dir_path, "Examples/simple_client.py") , "./simple_client.py")
-    except Exception:
-        print("files already exist...")
+            copyanything(
+                path.join(dir_path, "MultiClient/__client_config__"),
+                "./__client_config__",
+            )
+            shutil.copyfile(
+                path.join(dir_path, "Examples/simple_client.py"), "./simple_client.py"
+            )
+    except Exception as ex:
+        print("files already exist...", ex)
+    else:
+        print("create project successfuly")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
